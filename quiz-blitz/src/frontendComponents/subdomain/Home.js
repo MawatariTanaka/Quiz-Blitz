7307e9e44b8697a1df71be9660babaf698a3dd97
MawatariTanaka <buinhatminh2005@gmail.com>
2022-05-18 23:02:57 +0700

    Update quiz

--- quiz-blitz/src/frontendComponents/subdomain/Home.js
+++ quiz-blitz/src/frontendComponents/subdomain/Home.js
@@ -3,24 +3,51 @@ import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../backendComponents/firebaseConfig';
 
 export default function Home() {
+    //Temporary variable
+    const [a, setA] = useState(0);
+
     const [quizList, setQuizList] = useState([]);
+    const [question, setQuestion] = useState('');
+    const [answers, setAnswers] = useState([]);
+    const [answersJSX, setAnswersJSX] = useState([]);
     const quizCollectionReference = collection(db, 'questions');
     useEffect(() => {
+        const updateAnswer = (ans) => {
+            if (ans) {
+                if (ans.length > 0) {
+                    return ans.map((answer) => (
+                        <div className="daily-answer-container">{answer}</div>
+                    ));
+                }
+            }
+        };
         const getQuiz = async () => {
             const data = await getDocs(quizCollectionReference);
             setQuizList(
                 data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
             );
-            console.log(quizList);
+            const todayQuiz =
+                quizList[Math.floor(Math.random() * quizList.length)];
+            if (!todayQuiz) {
+                setA(a + 1);
+            } else if (todayQuiz.question !== '') {
+                console.log(todayQuiz);
+                setQuestion(todayQuiz.question);
+                setAnswers(todayQuiz.answers);
+                console.log(question);
+                console.log(answers);
+            }
         };
+
         getQuiz();
-    }, []);
+    }, [a]);
     return (
         <main className="home-main">
-            <section className="daily-question">
+            <section className="daily-quiz">
                 <h2>Câu Hỏi Trong Ngày</h2>
-                <div className="daily-question-container">
-                    <div className="question-container"></div>
+                <div className="daily-quiz-container">
+                    <div className="question-container">{question}</div>
+                    <div className="answer-container">{answersJSX}</div>
                 </div>
             </section>
         </main>
